f4531714559445d3090168a31375b0c374c6255b
dpbam <derekphtpt@gmail.com>
2022-02-17 12:18:37 -0700

    close

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@
 // Print the respective minimum and maximum values as a single line of two space-separated long integers
 // console.log(minSumOfFour, maxSumOfFour)
 
-const arr = [3, 1, 5, 6, 2];
-let largest = 0;
-let smallest = arr[0];
+let arr = [3, 1, 5, 6, 2];
+// order the array
+let orderedArr = arr.sort();
 // need to iterate over the array and find the 4 smallest numbers
 // maybe eliminate the largest number of the original array
 // make a new array of those numbers
@@ -15,48 +15,25 @@ let smallest = arr[0];
 // make a new array of those numbers
 // sum all of those numbers in that new array
 
-miniMaxSum(arr);
-function miniMaxSum(arr) {
-  for (let i = 0; i < 5; i++) {
-    // find the largest number
-    //   compare 'largest' to the first number in the array
-    // if the first number is larger than 'largest' then THAT becomes 'largest
-    if (largest < arr[i]) {
-      largest = arr[i];
-    }
-    // find the smallest number
-    // compare 'smallest' to the first number in the array
-    // if the first number is smaller than 'smallest'
-    if (arr[i] < smallest) {
-      smallest = arr[i];
-    }
-  }
-  console.log('smallest', smallest);
-  console.log('largest', largest);
-
-  //   now need to take 'largest' out of the original array and make a new array
-  let newLargeArr = [];
-  newLargeArr = arr.slice(largest);
-  console.log(newLargeArr);
-  //   then add the numbers in the newLargeArray together
-//   let newLargeArraySum = newlargeArray[0] + newlargeArray[1] + newlargeArray[2] + newlargeArray[3];
-  let initialLargeSum = 0;
-  newLargeArr.reduce //but not sure how to write this
-console.log()
-  //   now need to take 'smallest' out of the original array and make a new array
-  let newSmallArr = [];
-  newSmallArr = arr.slice(smallest);
-  console.log(newSmallArr);
-
-//   then add the numbers in the newSmallArray together
-newSmallArr.reduce //but not sure how to write this
 // OR
-let newSmallArraySum =  function() {
-    // newSmallArray[0] + newSmallArray[1] + newSmallArray[2] + newSmallArray[3]
-    smallSum = 0;
-    for(let i = 0;i < newSmallArray.length; i++) {
-        smallSum = smallSum++;
-    };
 
-};
-console.log(smallSum);
+function miniMaxSum(orderedArr) {
+  // then eliminate the final number and the first number...
+
+  let maxArr = orderedArr.slice(1);
+  let minArr = orderedArr.slice(0, -1);
+
+  console.log(maxArr, minArr);
+
+  var maxSum = 0;
+
+  for (let i = 0; i < maxArr.length; i++) {
+    maxSum += maxArr[i];
+  }
+  var minSum = 0;
+  for (let i = 0; i < minArr.length; i++) {
+    minSum += minArr[i];
+  }
+  console.log(minSum, maxSum);
+}
+miniMaxSum(arr);
